5f49dcfeca32719a854efe8f15cac842c1b3366c
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-11 17:13:49 +0000

    refactor into Post component

--- pages/blog/[title].tsx
+++ pages/blog/[title].tsx
@@ -1,29 +1,16 @@
-import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { fetchEntryByTitle } from '../../components/posts/postsAPI'
-import { Entry } from 'contentful'
+import Post from '../../components/posts/Post'
 import Layout from '../../components/Layout'
 
-const Post = () => {
+const BlogPage = () => {
   const router = useRouter()
   const { title } = router.query
 
-  const [post, setPost] = useState<Entry<any>>()
-
-  useEffect(() => {
-    async function getPosts() {
-      const post = await fetchEntryByTitle(title as string)
-      setPost(post)
-    }
-    getPosts()
-  }, [])
-
   return (
     <Layout>
-      <p>Post: {title}</p>
-      <p>Alt: {post == undefined ? "not loaded" : post.fields.alt}</p>
+      <Post title={title as string} />
     </Layout>
   )
 }
 
-export default Post
+export default BlogPage
