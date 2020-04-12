50bfe41c691b04ad3ca3fe17a6d25a7d586b5c8c
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-12 21:58:35 +0000

    Fixed style in AboutMe component

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,8 +1,8 @@
 const AboutMe = () => {
     return (
         <>
-            <h2>Welcome</h2>
-            <div style={{display: 'flex'}}>
+            <h2 style={{marginBottom: '0px'}}>Welcome</h2>
+            <div style={{display: 'flex', alignItems: 'center'}}>
             <p style={{}}>
                 Hello! I'm John and welcome to my website.<br/>
                 It's a collection of my thoughts and musings. Please enjoy!
