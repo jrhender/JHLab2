fd44a382f8f0ca72754ac38ba2f10126bd6291b6
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-12 01:20:12 +0000

    Convert to PostPreview.module.css

--- components/posts/PostPreview.tsx
+++ components/posts/PostPreview.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import styles from './PostPreview.module.css'
 
 const layoutStyle = {
   marginBottom: 5,
@@ -11,47 +12,14 @@ function PostPreview({ alt, date, image, imageAttribution, title, url }) {
     <div style={layoutStyle}>
       <Link href="/blog/[title]" as={`/blog/${title}`}>
         <a>
-          <div className="container">
-            <div className="image">
+          <div className={styles.container}>
+            <div className={styles.image}>
               <img title={imageAttribution} alt={alt} src={image} />
             </div>
-            <div className="text">
+            <div className={styles.text}>
               <h2>{title}</h2>
               <h4>{date}</h4>
             </div>
-            <style jsx>{`
-            .image {
-              margin: auto auto;
-            }
-            .container {
-              display: grid;
-              grid-template-columns: 1fr 1fr;
-              height: auto;
-            }
-            a {
-              border-bottom: none;
-            }
-            a:hover {
-              border-bottom: none;
-            }
-            .text {
-              padding: 5px;
-            }
-            h2 {
-              color: black;
-              font-size: 24px;
-              margin-bottom: 0;
-            }
-            h4 {
-              color: black;
-              font-size: 16px;
-              font-weight: 500;
-              margin-top: 8px;
-            }
-            img {
-              width: auto;
-            }
-          `}</style>
           </div>
         </a>
       </Link>
