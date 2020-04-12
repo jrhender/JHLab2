78ecaefa31819f62ff33a18baaea6d6624065f18
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-12 04:08:36 +0000

    Fixed footer images in posts

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <div className={styles.AppFooter}>
             <a href="https://twitter.com/JohnRTHenderson" className={styles.twitterLogo}>
-                <img src='footer/Twitter_Logo_White_On_Blue.svg' alt='Twitter logo' />
+                <img src='/Twitter_Logo_White_On_Blue.svg' alt='Twitter logo' />
             </a>
             <a href="https://www.linkedin.com/in/jhend/" className={styles.linkedinLogo}>
-                <img src='footer/In-2CRev-28px-R.png' alt='LinkedIn logo'/>
+                <img src='/In-2CRev-28px-R.png' alt='LinkedIn logo'/>
             </a>
         </div>
     )
