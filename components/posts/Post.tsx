a5837aea6f8d213d55a4c1afd02b66722b477bf1
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-12 02:36:38 +0000

    moved the useEffect query back to Post.tsx

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react'
+import { fetchEntryByTitle } from '../../components/posts/postsAPI'
+import { Entry } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 const layoutStyle = {
@@ -7,14 +10,35 @@ const layoutStyle = {
 };
 
 const Post = (props) => {
-  let post = props.post
-  return (
-    <div style={layoutStyle}>
-      <h1>{props.title}</h1>
+  const title = props.title
+  const [post, setPost] = useState<Entry<any>>()
+
+  useEffect(() => {
+    async function getPosts() {
+      const post = await fetchEntryByTitle(title as string)
+      console.log(post)
+      setPost(post)
+    }
+    getPosts()
+  }, [])
+
+  let postMarkup
+  if (post == undefined) {
+    postMarkup = <p></p>
+  }
+  else {
+    console.log(title)
+    postMarkup =
       <>
+        <h1>{title}</h1>
         <p>{post.fields.date}</p>
         <div>{documentToReactComponents(post.fields.body)}</div>
       </>
+  }
+
+  return (
+    <div style={layoutStyle}>
+      {postMarkup}
     </div>
   )
 }
--- pages/blog/[title].tsx
+++ pages/blog/[title].tsx
@@ -1,42 +1,18 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { fetchEntryByTitle } from '../../components/posts/postsAPI'
-import { Entry } from 'contentful'
 import Post from '../../components/posts/Post'
 import Layout from '../../components/Layout'
 
-const BlogPage = () => {
-  const router = useRouter()
-  const { title } = router.query
-
-  const [post, setPost] = useState<Entry<any>>()
-
-  useEffect(() => {
-    async function getPosts() {
-      const post = await fetchEntryByTitle(title as string)
-      console.log(post)
-      setPost(post)
-    }
-    getPosts()
-  }, [])
-
-  let postMarkup
-  if (post == undefined) {
-    postMarkup = <p>not loaded</p>
-  }
-  else {
-    postMarkup = <Post title={title as string} post={post} />
-  }
-
+const BlogPage = (props) => {
   return (
     <Layout>
-      {postMarkup}
+      <Post title={props.title as string} />
     </Layout>
   )
 }
 
-BlogPage.getInitialProps = ({query}) => ({
-  key: query.title
+BlogPage.getInitialProps = ({ query }) => ({
+  key: query.title,
+  title: query.title
 })
 
 export default BlogPage
