c27afc794fe6eca6151555ea2f5f3f2edd847d69
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-12 03:39:33 +0000

    added Footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import './../styles/Footer.css';
+//import twitterLogo from './../images/Twitter_Logo_White_On_Image.svg';
+import twitterLogo from './../images/Twitter_Logo_White_On_Blue.svg';
+import linkedinLogo from './../images/In-2CRev-28px-R.png';
+
+const Footer = (props) => {
+    return (
+        <div className="App-footer">
+            <p>Connect:</p>
+            <a href="https://twitter.com/JohnRTHenderson" className='twitter-logo'>
+                <img src={twitterLogo} />
+            </a>
+            <a href="https://www.linkedin.com/in/jhend/" className='linkedin-logo'>
+                <img src={linkedinLogo} />
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
