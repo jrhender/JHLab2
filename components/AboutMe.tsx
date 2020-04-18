1a7958d68b7e381185407fbdf0a0d0529a46cd74
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-18 22:15:42 +0000

    Changed musings to learnings

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
             <div style={{display: 'flex', alignItems: 'center'}}>
             <p style={{}}>
                 Hello! I'm John and welcome to my website.<br/>
-                It's a collection of my thoughts and musings. Please enjoy!
+                It's a collection of my thoughts and learnings. Please enjoy!
             </p>
             <img src='/myFace.jpg' style={{height: '80px', borderRadius: '40%', marginLeft: '10px'}}/>
             </div>
