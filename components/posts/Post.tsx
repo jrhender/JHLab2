fb98a8f6f4301fe099a58b0d4883ad847472ca7d
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-04-25 19:43:56 +0000

    removed unneeded code from Post

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -4,24 +4,13 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import styles from './Post.module.css'
 
 const Post = (props) => {
-  let postMarkup
   let post = props.post
-  if (post == undefined) {
-    postMarkup = <p></p>
-  }
-  else {
-    postMarkup =
-      <>
+  return (
+    <div className={styles.container}>
         <h1>{post.fields.title}</h1>
         {/* <p>Created: {post.sys.createdAt.split('T')[0]}</p> */}
         <p>Last updated: {post.sys.updatedAt.split('T')[0]}</p>
         <div>{documentToReactComponents(post.fields.body)}</div>
-      </>
-  }
-
-  return (
-    <div className={styles.container}>
-      {postMarkup}
     </div>
   )
 }
