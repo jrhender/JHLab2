ff65c54de6c6e80ceda3ac485faf1d384bd6b10d
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-06-20 20:59:24 +0000

    Added image captions

--- components/posts/ContentfulRichText.tsx
+++ components/posts/ContentfulRichText.tsx
@@ -2,22 +2,27 @@
 import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import React from "react";
+import styles from './Post.module.css'
 
 const richTextOptions = {
   renderNode: {
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      console.log(node);
       const { title, description, file } = node.data.target.fields;
       const mimeType = file.contentType
       const mimeGroup = mimeType.split('/')[0]
 
       switch (mimeGroup) {
         case 'image':
-          return <img
-            title={ title ? title['en-US'] : null}
-            alt={description ?  description['en-US'] : null}
-            src={file.url}
-            style={{maxWidth:'100%'}}
-          />
+          return <div>
+            <img className={styles.image}
+              title={title}
+              alt={description}
+              src={file.url}
+              style={{maxWidth:'100%'}}
+            />
+            <p className={styles.imageCaption}>{description}</p>
+          </div>
         default:
           return <span style={{backgroundColor: 'red', color: 'white'}}> {mimeType} embedded asset </span>
       }
