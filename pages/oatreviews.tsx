242897f101d8a55bf3591e19ad98bbca97d75342
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-06-29 04:54:51 +0000

    added oat reviews image

--- pages/oatreviews.tsx
+++ pages/oatreviews.tsx
@@ -3,6 +3,7 @@ import Layout_oat from '../components_oat/Layout_oat'
 function OatReviews(props) {
   return (
     <Layout_oat>
+        <img src='/earths_own_oat_milk_800x.png' />
     </Layout_oat>
   )
 }
