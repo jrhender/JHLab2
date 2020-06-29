4c00f7db424d77d79844ca722222e2438fe0f4aa
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-06-29 05:17:58 +0000

    Fixed width issue and added oat is life

--- components_oat/footer/Footer_oat.tsx
+++ components_oat/footer/Footer_oat.tsx
@@ -3,6 +3,7 @@ import styles from './Footer_oat.module.css';
 const Footer = () => {
     return (
         <div className={styles.AppFooter}>
+            Oat is life
         </div>
     )
 };
--- pages/oatreviews.tsx
+++ pages/oatreviews.tsx
@@ -2,9 +2,11 @@ import Layout_oat from '../components_oat/Layout_oat'
 
 function OatReviews(props) {
   return (
-    <Layout_oat>
-        <img src='/earths_own_oat_milk_800x.png' />
-    </Layout_oat>
+      <div style={{display:'flex'}}>
+        <Layout_oat>
+            <img src='/earths_own_oat_milk_800x.png' />
+        </Layout_oat>
+      </div>
   )
 }
 
