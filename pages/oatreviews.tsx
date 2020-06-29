03b48cf0989713908be8663e08ffc67b83af66cc
jrhender <Henderson@instance-1.us-west1-b.c.jh-personal-site.internal>
2020-06-29 06:03:14 +0000

    Added Chobani

--- pages/oatreviews.tsx
+++ pages/oatreviews.tsx
@@ -2,11 +2,14 @@ import Layout_oat from '../components_oat/Layout_oat'
 
 function OatReviews(props) {
   return (
-      <div style={{display:'flex'}}>
-        <Layout_oat>
-            <img src='/earths_own_oat_milk_800x.png' />
-        </Layout_oat>
-      </div>
+    <Layout_oat>
+        <h2>Earth's Own</h2>
+        <p>Earth's Own is really good. 5 stars out of 5</p>
+        <img src='/earths_own_oat_milk_800x.png' />
+        <h2>Chobani</h2>
+        <p>Chobani is top notch. "Like drinking liquid ice cream"</p>
+        <img src='/ChobaniOatDrinks_Lead.jpg' />
+    </Layout_oat>
   )
 }
 
